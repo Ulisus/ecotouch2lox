ef6c894aa7280426c69734c3b85c29fddc591b07
konkz <54497015+konkz@users.noreply.github.com>
2019-08-25 11:46:26 +0200

    Delete config.php

--- ecotouch2lox-php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// tested with Waterkotte WWPR2
-
-define(WK_IP, ""); // ip address of Waterkotte heat pump
-define(WK_USER, 'waterkotte'); // username of web interface
-define(WK_PWD, 'waterkotte'); // password of web interface
-
-?>
